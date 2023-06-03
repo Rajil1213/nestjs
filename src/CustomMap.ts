d8df7f5eb20617f718c4e60d28918a564691b9bd
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-03 16:04:19 +0545

    Create custom map class for google.maps

---
+++ src/CustomMap.ts
@@ -0,0 +1,13 @@
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementID: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementID) as HTMLElement, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /// <reference types="@types/google.maps" />
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 import { User } from "./user";
 
 const user = new User();
@@ -8,10 +9,4 @@ console.log(user);
 const company = new Company();
 console.log(company);
 
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+new CustomMap("map");
