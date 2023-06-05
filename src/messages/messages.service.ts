2c689f47b87bc6d20fc4ce1f0636864a3a3d9190
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-05 17:13:41 +0545

    Create service and repository; and attach to the controller

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from './message.repository';
+
+export class MessagesService {
+  protected messagesRepo: MessagesRepository;
+
+  //! use dependency injection instead!
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
