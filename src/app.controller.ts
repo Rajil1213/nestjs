35cf7938759469bb6d63b35b1c9f1ab8ed75fee5
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-05 13:57:47 +0545

    Add routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 
 // decorator to handle routing
-@Controller()
+@Controller("app")
 export class AppController {
   // create methods for routes and specify the method via another decorator
-  @Get()
+  @Get("/hello")
   getRootRoute() {
     return "Hi, there!";
+  }
+
+  @Get("/bye")
+  getByeRoute() {
+    return "Bye, bye";
   }
 }
