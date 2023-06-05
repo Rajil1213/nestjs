cd83ea1f198fa22be2b9a4707828c3a9ffa2d043
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-05 16:41:21 +0545

    Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
-import { AppModule } from './app.module';
+import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
+    MessagesModule,
     new FastifyAdapter(),
   );
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
