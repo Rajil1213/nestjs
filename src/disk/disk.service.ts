f3869a2eeb5ea692bfd99f11b93bfd3ee3ff9b43
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-06 14:22:09 +0545

    Import Power in the Disk module and use service

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PowerService } from "src/power/power.service";
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  writeToDisk(content: string) {
+    console.log(
+      "Drawing 15 watts of power from Power Service to write to disk",
+    );
+    this.powerService.supplyPower(15);
+    return content;
+  }
+}
