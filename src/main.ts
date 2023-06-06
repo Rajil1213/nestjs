21227c358e0015d4ebfdb05fe45179c2cf0afb2f
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-06 20:16:22 +0545

    Add ValidationPipe with whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
   );
   await app.listen(3000);
 }
