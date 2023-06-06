1d0f0980e42bfc49d30de91c7f1bad4a8ef26596
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-06 15:41:56 +0545

    Create sqlite db connection to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ReportsModule } from './reports/reports.module';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ReportsModule } from "./reports/reports.module";
+import { UsersModule } from "./users/users.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    ReportsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
