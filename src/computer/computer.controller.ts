adee11a4430b34435084da01445cd38a91c9f011
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-06 14:30:40 +0545

    Setup DI between controller and cpu/disk services

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { DiskService } from "./../disk/disk.service";
+import { Controller, Get } from "@nestjs/common";
+import { CpuService } from "src/cpu/cpu.service";
 
-@Controller('computer')
-export class ComputerController {}
+@Controller("computer")
+export class ComputerController {
+  constructor(
+    private cpuService: CpuService,
+    private diskService: DiskService,
+  ) {}
+
+  @Get()
+  run() {
+    const result = this.cpuService.compute(1, 5);
+    const diskWriteResult = this.diskService.writeToDisk(`${result}`);
+
+    return [result, diskWriteResult];
+  }
+}
