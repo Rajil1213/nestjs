f25c377447acfb8a2303002aefda86cd18eca52b
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-08 15:06:06 +0545

    Create signup service

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import * as bcrypt from "bcrypt";
+
+import {
+  BadRequestException,
+  Injectable,
+} from "@nestjs/common";
 
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see if an email already exists
+    const users = await this.usersService.find(email);
+    if (users.length > 0) {
+      throw new BadRequestException("email already in use");
+    }
+
+    const rounds = 10; // generate salt automatically from these many rounds
+    const hashedPassword = await bcrypt.hash(password, rounds);
+
+    return this.usersService.create(email, hashedPassword);
+  }
 }
