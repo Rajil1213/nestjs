a37b1789120a9049ab1f00022e295ce1d1987e32
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-08 10:36:14 +0545

    Create controller to query users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  UnprocessableEntityException,
+  Query,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UsersService } from "./users.service";
 
@@ -9,5 +17,21 @@ export class UsersController {
   @Post("signup")
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get(":id")
+  // the id coming from the params is a string
+  findUser(@Param("id") id: string) {
+    const idAsNum = parseInt(id);
+    if (Number.isNaN(idAsNum)) {
+      throw new UnprocessableEntityException("param :id must be a number");
+    }
+
+    return this.usersService.findOne(idAsNum);
+  }
+
+  @Get()
+  findAllUsers(@Query("email") email: string) {
+    return this.usersService.find(email);
   }
 }
