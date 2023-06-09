0237b1ccc16d9a469e17c42533f78e21dc4f1ae0
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-09 21:09:54 +0545

    Sort imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ReportsModule } from "./reports/reports.module";
-import { UsersModule } from "./users/users.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./users/users.entity";
 import { Report } from "./reports/reports.entity";
+import { ReportsModule } from "./reports/reports.module";
+import { User } from "./users/users.entity";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
