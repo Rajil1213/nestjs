ff9e3d7f66b9ebfda8e4782475d199ca748ece17
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-12 17:24:06 +0545

    Add ref to user and dto when creating reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,17 +1,21 @@
-import { AuthGuard } from "src/guards/auth.guard";
-
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 
+import { AuthGuard } from "../guards/auth.guard";
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { CurrentUser } from "../users/decorators/current-user.decorator";
+import { User } from "../users/users.entity";
 import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportDto } from "./dtos/report.dto";
 import { ReportsService } from "./reports.service";
 
 @Controller("reports")
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @UseGuards(AuthGuard)
   @Post()
-  createReport(@Body() body: CreateReportDto) {
-    this.reportsService.create(body);
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
