6b563c29ced1ce48379d45ad388a1858cd0cb6d0
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-13 17:32:07 +0545

    Use typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module, ValidationPipe } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD, APP_PIPE } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import * as dbConfig from "../ormconfig.js";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CurrentUserGuard } from "./guards/current-user.guard";
-import { Report } from "./reports/reports.entity";
 import { ReportsModule } from "./reports/reports.module";
-import { User } from "./users/users.entity";
 import { UsersModule } from "./users/users.module";
 
 @Module({
@@ -17,19 +16,9 @@ import { UsersModule } from "./users/users.module";
       isGlobal: true, // use everywhere
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRoot(dbConfig),
     ReportsModule,
     UsersModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: "sqlite",
-          database: config.get<string>("DB_NAME"),
-          synchronize: true,
-          entities: [User, Report],
-        };
-      },
-    }),
   ],
   controllers: [AppController],
   providers: [
