8f301378f6652563be19a4811685ecf095c8bf4b
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-13 13:20:14 +0545

    Create an APP_GUARD to act like a middleware

---
+++ src/guards/current-user.guard.ts
@@ -0,0 +1,21 @@
+import { UsersService } from "src/users/users.service";
+
+import { CanActivate, ExecutionContext, Inject } from "@nestjs/common";
+
+export class CurrentUserGuard implements CanActivate {
+  constructor(@Inject(UsersService) private usersService: UsersService) {}
+
+  async canActivate(context: ExecutionContext): Promise<true> {
+    const request = context.switchToHttp().getRequest();
+
+    const userId = request.session.get("userId");
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user || {};
+    }
+
+    // always return true, handle Auth via AuthGuard for specific routes
+    return Promise.resolve(true);
+  }
+}
