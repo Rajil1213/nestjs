497b3e1bceef4788844b77054ccc765d64fad292
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-13 19:04:40 +0545

    Close connection after each test

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,6 @@
 import { rm } from "fs/promises";
 import { join } from "path";
+import { getConnection } from "typeorm";
 
 global.beforeEach(async () => {
   try {
@@ -8,4 +9,9 @@ global.beforeEach(async () => {
     // could be invoked if there is no file to delete but that is fine
     console.log(err);
   }
+});
+
+global.afterEach(async () => {
+  const conn = getConnection();
+  await conn.close();
 });
