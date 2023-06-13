163ede571b02f6b95b4a89735f01000dd83570c5
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-13 14:55:58 +0545

    Implement service to get estimate

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 
+import { GetEstimateDto } from "./dtos/create-estimate.dto";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { Report } from "./reports.entity";
 
@@ -21,5 +22,28 @@ export class ReportsService {
     const report = await this.repo.findOne({ where: { id } });
     report.approved = approve;
     return this.repo.save(report);
+  }
+
+  async createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    year,
+    mileage,
+  }: GetEstimateDto) {
+    return await this.repo
+      .createQueryBuilder("estimate")
+      .select("ROUND(AVG(price), 2)", "price")
+      .where("make = :make", { make }) // do not use raw string because SQLi
+      .andWhere("model = :model", { model })
+      .andWhere("lng - :lng BETWEEN -5 AND 5", { lng })
+      .andWhere("lat - :lat BETWEEN -5 AND 5", { lat })
+      .andWhere("year - :year BETWEEN -3 and 3", { year })
+      .andWhere("approved IS TRUE")
+      .orderBy("ABS(mileage - :mileage)", "DESC")
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
